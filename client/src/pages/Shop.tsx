60faea287ee1f67d05ef1880fd1b9c5bf63fd4d6
Bartek <bartek.kaszowski@spoko.pl>
2023-04-12 23:57:41 +0200

    create request with multipart/form-data to play with backend

--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import {useSelector} from "react-redux";
-import {RootState} from "../redux/store/store";
-
+import React, {useEffect} from 'react';
+import axios from 'axios';
+import {SubmitHandler, useForm} from "react-hook-form";
+type Inputs = {
+    test:string;
+}
 export const  Shop = () => {
-    const {amount} = useSelector((root:RootState) => root)
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    // const {amount} = useSelector((root:RootState) => root);
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get('http://localhost:3000/product/all');
+            console.log(response)
+        })()
+    }, [])
+    const onSubmit = async (data:any) => {
+        console.log('test')
+        const formData = new FormData();
+        console.log(watch('test'))
+        console.log(data.test)
+        if(!data.test[0])  throw new Error()
+        formData.append("image", data.test[0]);
+        const res = await axios.post('http://localhost:3000/product', formData, {
+            headers:{
+                'Content-type':'multipart/form-data'
+            }
+        });
+    }
     return(<>
-
-        <p>We found {amount} items for you</p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="file" {...register("test")}/>
+            <button type='submit'></button>
+        </form>
 
     </>)
 }
