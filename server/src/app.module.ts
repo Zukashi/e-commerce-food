866689af7a634f7f16b1600a6f40ef6359f0b154
Bartek <bartek.kaszowski@spoko.pl>
2023-04-12 13:16:47 +0200

    add aws s3

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { ProductModule } from './product/product.module';
 import { Product } from './product/entities/product.entity';
+import { AwsModule } from './aws/aws.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -18,6 +20,7 @@ import { Product } from './product/entities/product.entity';
       synchronize: true,
     }),
     ProductModule,
+    AwsModule,
   ],
   controllers: [],
   providers: [],
