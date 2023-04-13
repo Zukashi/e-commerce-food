d11a36bc44cce0e95a0bc9ef9ba3a3ff28fe108c
Bartek <bartek.kaszowski@spoko.pl>
2023-04-13 23:42:08 +0200

    refresh token patch register post  login actions

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
+import { Public } from './decorator/public.decorator';
+import { SignUpDto } from './dto/signUp.dto';
+import { SignInDto } from './dto/signIn.dto';
+import { Cookies } from './decorator/cookie.decorator';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async signIn(@Body() signInDto: SignInDto, @Res() res: Response) {
+    console.log(2345);
+    await this.authService.signIn(signInDto, res);
+  }
+
+  @Public()
+  @HttpCode(HttpStatus.CREATED)
+  @Post('register')
+  async register(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+
+  @Public()
+  @Patch('refreshToken')
+  async refreshToken(
+    @Cookies('refresh_token') refresh_token: string,
+    @Res() res: Response,
+  ) {
+    return this.authService.refreshToken(refresh_token, res);
+  }
+}
