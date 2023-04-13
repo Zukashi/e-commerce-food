4756aa3fe3ff09b5d828755c52ec3cf8dd4ef188
Bartek <bartek.kaszowski@spoko.pl>
2023-04-13 23:48:31 +0200

    axios setup to apply DRY principle

---
+++ client/src/api/axios.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import {apiUrl} from "../config/api";
+const BASE_URL = apiUrl;
+
+export default axios.create({
+    baseURL: BASE_URL
+});
+
+export const axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
+});
