e7dd647dcf496a16f07d58ae8398ceb27c274049
Bartek <bartek.kaszowski@spoko.pl>
2023-04-13 23:46:09 +0200

    change regular axios fetches to axiosPrivate to apply useAxiosPrivate interceptors

--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -1,18 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {SubmitHandler, useForm} from "react-hook-form";
+import {useAxiosPrivate} from "../hooks/use-axios-private";
 type Inputs = {
     test:string;
 }
 export const  Shop = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-    const [currentImage, setCurrentImage] = useState<string>('')
+    const [currentImage, setCurrentImage] = useState<string>('');
+    const axiosPrivate = useAxiosPrivate()
     // const {amount} = useSelector((root:RootState) => root);
     const onSubmit = async (data:any) => {
         const formData = new FormData();
         if(!data.test[0])  throw new Error()
         formData.append("image", data.test[0]);
-        const res = await axios.patch('http://localhost:3000/product-image/5f23eb0c-bc2f-4992-9403-fd0b12ea5376', formData, {
+        const res = await axiosPrivate.patch('product-image/5f23eb0c-bc2f-4992-9403-fd0b12ea5376', formData, {
             headers:{
                 'Content-type':'multipart/form-data'
             }
@@ -20,10 +22,10 @@ export const  Shop = () => {
     }
     useEffect(() => {
         (async () => {
-            const response = await axios.get('http://localhost:3000/product-image/all');
+            const response = await axiosPrivate.get('product-image/all');
             setCurrentImage(response.data[0])
         })()
-    }, [onSubmit])
+    }, [])
 
     return(<>
         <form onSubmit={handleSubmit(onSubmit)}>
