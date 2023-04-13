3f5f7a490cf323269ca992f22006bf148349581c
Bartek <bartek.kaszowski@spoko.pl>
2023-04-13 23:43:39 +0200

    UserEntity

---
+++ server/src/user/entities/user.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    nullable: true,
+  })
+  refresh_token: string;
+}
