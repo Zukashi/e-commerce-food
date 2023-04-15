9600dbe9e1a978d7d3c7cbbb3d5457467e9e0771
Bartek <bartek.kaszowski@spoko.pl>
2023-04-15 23:56:12 +0200

    new nav icon

--- client/src/components/nav/Nav.tsx
+++ client/src/components/nav/Nav.tsx
@@ -4,6 +4,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import CompareArrowsOutlinedIcon from '@mui/icons-material/CompareArrowsOutlined';
 import {DrawerComponent} from "./DrawerComponent";
 import {useDispatch} from "react-redux";
 import {setDrawer} from "../../redux/nav";
@@ -14,10 +15,16 @@ export const Nav = () => {
         <nav className='navbar'>
         <div className='container'>
             <MenuIcon className='icon ' onClick={() => dispatch(setDrawer(true))} fontSize={'large'}/>
+            <div className="input-container"></div>
             <div className='right-side-icons-container'>
-                <div className='icon-container'>
-                    <FavoriteBorderOutlinedIcon fontSize={'large'}/>
+                <div className='fade-container'>
+                    <CompareArrowsOutlinedIcon fontSize={'large'}/>
+                    <p className='nav-item-text'>Compare</p>
                 </div>
+                   <div className='fade-container'>
+                       <FavoriteBorderOutlinedIcon fontSize={'large'}/>
+                       <p className='nav-item-text'>Wishlist</p>
+                   </div>
                <div className='fade-container '>
                    <div className='account-dropdown dropdown-animate cart'>
                        <ul className='account-list cart-list'>
@@ -39,9 +46,8 @@ export const Nav = () => {
                            <button className='primary-button' >Checkout</button>
                        </div>
                    </div>
-                   <div className='trigger-fade'>
-                       <ShoppingCartOutlinedIcon fontSize={'large'}/>
-                   </div>
+                   <ShoppingCartOutlinedIcon className={'trigger-fade'} fontSize={'large'}/>
+                   <p className="nav-item-text">Cart</p>
                </div>
                 <div className='fade-container'>
                     <div className='account-dropdown dropdown-animate'>
@@ -54,7 +60,8 @@ export const Nav = () => {
                             <li className='list-item'>Sign out</li>
                         </ul>
                     </div>
-                    <AccountCircleIcon fontSize={'large'} className={'trigger-fade'} onMouseEnter={() => setAccountHover(true)} onMouseLeave={() => setAccountHover(false)}/>
+                    <AccountCircleIcon fontSize={'large'} className={'trigger-fade'} />
+                    <p className='nav-item-text'>Account</p>
                 </div>
             </div>
         </div>
