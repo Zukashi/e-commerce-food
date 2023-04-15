83b076ba00be0dc24ee60dd7c258d07066db1474
Bartek <bartek.kaszowski@spoko.pl>
2023-04-15 23:55:45 +0200

    login component

---
+++ client/src/components/login/login.tsx
@@ -0,0 +1,75 @@
+import {SubmitHandler, useForm} from "react-hook-form";
+import {useAxiosPrivate} from "../../hooks/use-axios-private";
+import React from "react";
+import {Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Radio, RadioGroup, TextField} from "@mui/material";
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import {toast} from "react-toastify";
+import {useNavigate} from "react-router-dom";
+import './login.scss'
+type Login  = {
+    usernameOrEmail:string;
+    password:string;
+}
+const schema = yup.object().shape({
+
+    usernameOrEmail: yup.string().required(),
+    password: yup.string().min(8).max(20).required(),
+
+});
+export const Login = () => {
+    const {register, handleSubmit, formState:{errors}} = useForm<Login>({
+        resolver:yupResolver(schema)
+    });
+    const {register:registerRemember} = useForm();
+    const navigate = useNavigate();
+    const axios = useAxiosPrivate();
+    const onSubmit: SubmitHandler<Login> = async data => {
+
+
+        data.usernameOrEmail.includes('@')
+        try{
+            await axios.post('auth/login', {
+                    password:data.password,
+                ...(data.usernameOrEmail.includes('@') ? {email: data.usernameOrEmail} : {username:data.usernameOrEmail})
+            });
+            navigate('/')
+            toast.success('Logged successfully', {
+                position:"top-right",
+                theme:'dark',
+                autoClose:1500,
+            })
+        }catch(e:any){
+            toast.error(e.response.data.message, {
+                position:"top-right",
+                theme:'dark'
+            })
+        }
+    }
+
+    type registerType = "usernameOrEmail" | "password"
+    const labels = [['Username or Email', 'usernameOrEmail'], ['Password', 'password']]
+    return (<>
+
+        <section className='register-section'>
+            <div className="image-container">
+                <img color='register-image' src="https://themes.pixelstrap.com/fastkart/assets/images/inner-page/log-in.png" alt="image showing man using laptop indicating authentication"/>
+            </div>
+            <div className='bg-form'>
+                <div className='form-box'>
+                    <h3>Login</h3>
+                    <form className='register-form' onSubmit={handleSubmit(onSubmit)} autoComplete={'off'}>
+                        {labels.map((label,i) => <span key={label[1]}><TextField type={i > 0 ? 'password' : 'text' }  {...register(label[1] as registerType)}  label={label[0]} variant="outlined"/><p className='error-message'>{errors[(label[1] as registerType)]?.message} </p></span>)}
+
+                        <FormGroup>
+                            <FormControlLabel control={<Checkbox     {...registerRemember('rememberMe', {})}/>} label="Remember me" />
+
+                        </FormGroup>
+                        <button type={"submit"}  className='submit-button'>Login </button>
+                    </form>
+
+                </div>
+            </div>
+        </section>
+    </>)
+}
