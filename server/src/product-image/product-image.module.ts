2d26a89283158986911cef2fa92a10b6964e7bcf
Bartek <bartek.kaszowski@spoko.pl>
2023-04-16 23:52:46 +0200

    app guard

--- server/src/product-image/product-image.module.ts
+++ server/src/product-image/product-image.module.ts
@@ -4,6 +4,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductImageService } from './product-image.service';
 import { ProductImageController } from './product-image.controller';
 import { AwsModule } from '../aws/aws.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from '../auth/guards/access-token.guard';
+// import { AuthGuard } from '../auth/auth.guard';
 
 @Module({
   imports: [
