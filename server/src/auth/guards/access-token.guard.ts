88a7b233d1017b0005e1c9935928732137c05915
Bartek <bartek.kaszowski@spoko.pl>
2023-04-18 19:45:22 +0200

    access token guard return true if is public

--- server/src/auth/guards/access-token.guard.ts
+++ server/src/auth/guards/access-token.guard.ts
@@ -8,14 +8,23 @@ import { ConfigService } from '@nestjs/config';
 import { User } from '../../user/entities/user.entity';
 const jwt = require('jsonwebtoken');
 import { Request } from 'express';
+import { IS_PUBLIC_KEY } from '../decorator/public.decorator';
+import { Reflector } from '@nestjs/core';
 export interface ReqWithUser extends Request {
   user: User;
 }
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
-  constructor() {}
+  constructor(private reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<any> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
     const ctx = context.switchToHttp();
     const request = ctx.getRequest();
     const checkJwt = () => {
@@ -24,7 +33,6 @@ export class AccessTokenGuard implements CanActivate {
         process.env.ACCESS_TOKEN_SECRET,
         (err: any, user: User) => {
           if (!err) {
-            request['user'] = user;
             return true;
           } else {
             throw new UnauthorizedException();
