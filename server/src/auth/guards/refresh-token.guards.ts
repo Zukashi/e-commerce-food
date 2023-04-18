4306c35fb8389a4a61d80398b44915ad48ce27af
Bartek <bartek.kaszowski@spoko.pl>
2023-04-18 19:42:26 +0200

    refresh token guard if is public return true immediately

--- server/src/auth/guards/refresh-token.guards.ts
+++ server/src/auth/guards/refresh-token.guards.ts
@@ -4,22 +4,31 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { User } from '../../user/entities/user.entity';
 const jwt = require('jsonwebtoken');
 import { Request } from 'express';
+import { IS_PUBLIC_KEY } from '../decorator/public.decorator';
+import { Reflector } from '@nestjs/core';
 export interface ReqWithUser extends Request {
   user: User;
 }
 @Injectable()
 export class RefreshTokenGuard implements CanActivate {
-  constructor() {}
+  constructor(private reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<any> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
     const ctx = context.switchToHttp();
     const request = ctx.getRequest();
-
     //verify jwt
+    console.log('aaaa');
     const checkJwt = () => {
       jwt.verify(
         request.cookies.refreshToken,
@@ -27,7 +36,6 @@ export class RefreshTokenGuard implements CanActivate {
         (err: any, user: User) => {
           if (!err) {
             //assign user to req user for request using access tokens
-            request['user'] = user;
             return true;
           } else {
             throw new UnauthorizedException();
--- server/src/product/product.module.ts
+++ server/src/product/product.module.ts
@@ -5,11 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { ProductImage } from '../product-image/entities/product-image.entity';
 import { ProductImageModule } from '../product-image/product-image.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from '../auth/guards/access-token.guard';
+import { UserModule } from '../user/user.module';
+import { User } from '../user/entities/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Product, ProductImage]),
+    TypeOrmModule.forFeature([Product, ProductImage, User]),
     ProductImageModule,
+    UserModule,
   ],
   providers: [ProductService],
   controllers: [ProductController],
