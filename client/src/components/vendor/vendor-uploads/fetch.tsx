f4f09c22d3d41aba0a873ca969dd8020b8c6729f
Bartek <bartek.kaszowski@spoko.pl>
2023-04-20 21:23:04 +0200

    separate fetch component for fetches in UploadsForm.tsx change it so that axiosPrivate is passed because without it correct refresh token revitalization won't work

--- client/src/components/vendor/vendor-uploads/fetch.tsx
+++ client/src/components/vendor/vendor-uploads/fetch.tsx
@@ -1,8 +1,7 @@
-import {useAxiosPrivate} from "../../../hooks/use-axios-private";
 import {UploadProductFormValues} from "./UploadsForm";
+import {AxiosInstance} from "axios";
 
-export const createProduct = async (data:UploadProductFormValues) => {
-    const axios = useAxiosPrivate()
-    const res = await axios.post('product', data);
+export const createProduct = async ({data, axiosPrivate}:{axiosPrivate:AxiosInstance,data:UploadProductFormValues}) => {
+    const res = await axiosPrivate.post('vendor/product', data);
     return res.data
 }
