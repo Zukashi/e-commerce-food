7c2be7745e2187c776d4dd05993117c4acca44e4
Bartek <bartek.kaszowski@spoko.pl>
2023-04-20 15:57:36 +0200

    Vendor Layout component because content in different sections should have same sizes

---
+++ client/src/components/vendor/vendor-introduction.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink} from 'react-router-dom';
+import './vendormain.scss'
+export const VendorIntroduction = () => {
+    return <>
+
+
+            <div className='vendor-introduction'>
+               <div className='vendor-introduction-content-container'>
+                   <img src="" alt=""/>
+                   <img src="" alt="   "/>
+                   <div className='vendor-nav'>
+                       <NavLink  to='dashboard'>Dashboard</NavLink>
+                       <NavLink  to='uploads'>Uploads</NavLink>
+                       <NavLink  to='settings'>Settings</NavLink>
+                   </div>
+               </div>
+            </div>
+
+
+
+    </>
+}
