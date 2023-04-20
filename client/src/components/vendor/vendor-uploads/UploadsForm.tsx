bd37a5e98408875c19d5cc0d3af21e18d4140fcc
Bartek <bartek.kaszowski@spoko.pl>
2023-04-20 20:16:42 +0200

    create types for submit and add react query useMutate

--- client/src/components/vendor/vendor-uploads/UploadsForm.tsx
+++ client/src/components/vendor/vendor-uploads/UploadsForm.tsx
@@ -5,6 +5,8 @@ import {useForm} from "react-hook-form";
 import './form.scss'
 import {yupResolver} from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import {useMutation} from "react-query";
+import {createProduct} from "./fetch";
 const schema = yup.object().shape({
 
     productName: yup.string().required('product name is required'),
@@ -14,14 +16,26 @@ const schema = yup.object().shape({
     tags: yup.string().optional(),
 
 });
+
+export type UploadProductFormValues = {
+    productName: string;
+    price: string;
+    category: string;
+    quantity: string;
+    tags?: string;
+};
+
 export const UploadsForm = () => {
-    const {register, formState:{errors}, handleSubmit} = useForm({
+    const {register, formState:{errors}, handleSubmit} = useForm<UploadProductFormValues>({
         resolver:yupResolver(schema)
     });
+    const createNewProduct  = useMutation({
+        mutationFn:createProduct
+    })
     type registerType = 'productName' | 'price' | 'quantity'
     const labels = [['Product Name', 'productName'], ['Price (In USD)', 'price'], ['Quantity', 'quantity'] ,['Product Tags', 'tags']]
-    const onSubmit = (data:any) => {
-        console.log(data)
+    const onSubmit = (data:UploadProductFormValues) => {
+        createNewProduct.mutate(data)
     }
     return (<>
        <section>
