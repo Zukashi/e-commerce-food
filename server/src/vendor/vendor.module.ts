34a86ed0732933b12e8e9632ca98b312019f34e5
Bartek <bartek.kaszowski@spoko.pl>
2023-04-21 22:42:24 +0200

    vendor imports

--- server/src/vendor/vendor.module.ts
+++ server/src/vendor/vendor.module.ts
@@ -3,9 +3,18 @@ import { VendorController } from './vendor.controller';
 import { VendorService } from './vendor.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from '../product/entities/product.entity';
+import { ProductImage } from '../product-image/entities/product-image.entity';
+import { AwsModule } from '../aws/aws.module';
+import { ProductImageModule } from '../product-image/product-image.module';
+import { ProductModule } from '../product/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [
+    TypeOrmModule.forFeature([Product, ProductImage]),
+    AwsModule,
+    ProductModule,
+    ProductImageModule,
+  ],
   controllers: [VendorController],
   providers: [VendorService],
 })
