b5f343891eb4dc3c0c111f2ae671902934531afe
Bartek <bartek.kaszowski@spoko.pl>
2023-04-21 17:34:28 +0200

    create image logic in sub image

--- client/src/components/vendor/vendor-uploads/one-sub-image.tsx
+++ client/src/components/vendor/vendor-uploads/one-sub-image.tsx
@@ -1,11 +1,26 @@
 import React, {useState} from 'react';
+import {useDispatch} from "react-redux";
+import {setSubImageReducer} from "../../../redux/vendor-uploads";
 
 export const  OneSubImage = () => {
-    const [isPicked, setIsPicked] = useState(false)
+    const [image, setImage] = useState('')
+    const dispatch = useDispatch();
+    const onImageChange = async (e:React.ChangeEvent<HTMLInputElement>) => {
+        if(e.target.files && e.target.files[0]){
+            const formData = new FormData();
+            console.log(e.target.files[0])
+            // const r = await fetch(URL.createObjectURL(e.target.files[0]));
+            // formData.append("image", await r.blob());
+            console.log(formData)
+            dispatch(setSubImageReducer(URL.createObjectURL(e.target.files[0])))
+
+            setImage(URL.createObjectURL(e.target.files[0]))
+        }
+    }
     return (<>
 
            <div className='main-img-container sub-img-container'>
-               {isPicked ? <img width={'100%'} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8UaO8zvh5DjMIZ3C-jUQyIdtnCH9VUBvPRCZIbf60YQ&s" alt=""/> : <div className='main-img-placeholder sub-image'>
+               {image ? <img width={'100%'} src={image} alt="123"/> : <div className='main-img-placeholder sub-image'>
                    <div className='text-inside-placeholder-container'>
                        <div className='dimensions-container sub-image'>
                            <p>765 X 850</p>
@@ -14,6 +29,7 @@ export const  OneSubImage = () => {
                </div>}
 
                    <div className='icon-edit-container sub-image' style={{width:'25px', height:'25px'}}>
+                       <input type="file" className="file-input" onChange={onImageChange}/>
                        <img src="https://cdn-icons-png.flaticon.com/512/2356/2356780.png" width={'15px'} alt="edit icon"/>
                    </div>
 
