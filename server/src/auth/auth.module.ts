7ae946ee152edbc836ae35d6db17cdcfdded6e46
Bartek <bartek.kaszowski@spoko.pl>
2023-04-25 23:43:35 +0200

    JWTmodule passport module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -5,14 +5,18 @@ import {
   RequestMethod,
 } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthenticationService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
 import { UserModule } from '../user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserMiddleware } from '../user/middleware/user.middleware';
 import { Vendor } from '../vendor/entities/vendor.entity';
 import { VendorModule } from '../vendor/vendor.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt-strategy';
 
 @Module({
   imports: [
@@ -20,16 +24,21 @@ import { VendorModule } from '../vendor/vendor.module';
     UserModule,
     ConfigModule,
     VendorModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
+        },
+      }),
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [AuthenticationService, LocalStrategy, JwtStrategy],
+  exports: [AuthenticationService],
 })
-export class AuthModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(UserMiddleware)
-      .exclude('/auth/login/:role')
-      .forRoutes(AuthController);
-  }
-}
+export class AuthModule {}
