3e4f9e76de208fea55ce137dfe0063fd50084483
Bartek <bartek.kaszowski@spoko.pl>
2023-04-25 18:02:09 +0200

    getImages in react query

--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -1,14 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import { useForm} from "react-hook-form";
 import {useAxiosPrivate} from "../hooks/use-axios-private";
+import {useQuery} from "react-query";
+import {AxiosInstance} from "axios";
+import {Loader} from "../components/loader/Loader";
 type Inputs = {
     test:string;
+}
+export const getImages = async (axiosPrivate:AxiosInstance) => {
+    return (await axiosPrivate.get('product-image/all')).data
+
 }
 export const  Shop = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
     const [currentImage, setCurrentImage] = useState<string>('');
     const axiosPrivate = useAxiosPrivate()
     // const {amount} = useSelector((root:RootState) => root);
+    const {isLoading, data} = useQuery('image/all', () => getImages(axiosPrivate))
     const onSubmit = async (data:any) => {
         const formData = new FormData();
         if(!data.test[0])  throw new Error();
@@ -27,14 +35,16 @@ export const  Shop = () => {
             console.log(response.data)
             setCurrentImage(response.data[0])
         })()
-    }, [])
-
+    }, []);
+    if(isLoading){
+        return <Loader/>
+    }
     return(<>
         <form onSubmit={handleSubmit(onSubmit)}>
             <input type="file" {...register("test")}/>
             <button type='submit'></button>
         </form>
-        <img src={`${(currentImage as any)?.imageUrl}`} alt="product img"/>
+        <img src={`${(data[0] as any)?.imageUrl}`} alt="product img"/>
 
     </>)
 }
