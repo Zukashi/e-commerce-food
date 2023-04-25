db46a9315ff2891f2d09dc494fb9ddb0bf85bb75
Bartek <bartek.kaszowski@spoko.pl>
2023-04-25 18:02:01 +0200

    ProtectedRoute.tsx

---
+++ client/src/auth/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import React, {ReactNode} from "react";
+import {Navigate, Outlet, Route, useLocation} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {RootState} from "../redux/store/store";
+
+export function ProtectedRoute({  requiredPermission }: { requiredPermission:string}) {
+    const location = useLocation();
+    const {user} = useSelector((root:RootState) => root.user)
+    return (user && user.role === requiredPermission ? (<Outlet/>) : (<Navigate to={"/login"} state={{from:location}}   />)
+
+
+
+    );
+}
