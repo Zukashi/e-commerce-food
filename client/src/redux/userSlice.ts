ebfe5fc4e3195368ffae5744129e03f2fe982458
Bartek <bartek.kaszowski@spoko.pl>
2023-04-25 18:03:49 +0200

    user slice add loading user when refreshing token

--- client/src/redux/userSlice.ts
+++ client/src/redux/userSlice.ts
@@ -1,14 +1,17 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit'
-
 export interface User {
     user:any;
     refresh_token:boolean | string | null,
     access_token:boolean | string,
+    isAuthenticated:boolean,
+    loading:boolean,
 }
 const initialState:User = {
     user:{},
     refresh_token:localStorage.getItem('refresh_token') || false,
-    access_token:localStorage.getItem('access_token') || false
+    access_token:localStorage.getItem('access_token') || false,
+    isAuthenticated:false,
+    loading:false,
 
 }
 
@@ -16,10 +19,14 @@ export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        setUser(state, action:PayloadAction<any>){
-            state.user = action.payload
+        setUser(state, action:PayloadAction<{user:User, isAuthenticated:boolean}>){
+            state.user = action.payload.user;
+            state.isAuthenticated = action.payload.isAuthenticated
+        },
+        setLoading(state, action:PayloadAction<boolean>){
+            state.loading = action.payload
         }
 
     }
 })
-export const { setUser } = userSlice.actions
+export const { setUser, setLoading } = userSlice.actions
