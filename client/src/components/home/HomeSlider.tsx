f55c1be6c3f17ea9e24cbfde1e63bf253af13983
Bartek <bartek.kaszowski@spoko.pl>
2023-04-27 18:20:25 +0200

    HomeSlider component

--- client/src/components/home/HomeSlider.tsx
+++ client/src/components/home/HomeSlider.tsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import {FormControlLabel, Radio, TextField} from "@mui/material";
 import {useForm} from "react-hook-form";
-import './HomeSlider.scss'
+import './HomeSlider.scss';
+import {ImageSlider} from "./ImageSlider";
+import { motion, AnimatePresence } from "framer-motion"
+export const slides = [
+    {
+        header:"Don't miss amazing grocery deals",
+        text:'Sign up for the daily newsletter',
+        img:'https://wp.alithemes.com/html/nest/demo/assets/imgs/slider/slider-1.png'
+
+    },
+    {
+        header:'Fresh vegetables big discount',
+        text:'Save up to 50% on your first order',
+        img :'https://wp.alithemes.com/html/nest/demo/assets/imgs/slider/slider-2.png'
+},
+    {
+        header:'Test 3',
+        text:'Save up to 50% on your first order',
+        img :'https://wp.alithemes.com/html/nest/demo/assets/imgs/slider/slider-2.png'
+    }
+]
+export type Slides = typeof slides
 export const HomeSlider = () => {
-    const {handleSubmit, register} = useForm<{
-        email:string
-    }>()
+
     return (<>
-        <div className='slider-container'><div className='hero'>
-
-            <div className='slider'>
-                <div className='slider-content'>
-                    <h2>Don't miss amazing grocery deals</h2>
-
-                    <p>Sign up for the daily newsletter</p>
-
-
-                    <form className={'form'} >
-                        <TextField className='input'   {...register('email')}  label='Your email address' variant="outlined"/>
-                        <button className='submit-button newsletter'>Subscribe</button>
-                    </form>
-                    <ul className='dots'>
-                        <li className='dott'>1</li>
-                        <li className='dott'>2</li>
-                    </ul>
-                </div>
-            </div>
+        <AnimatePresence>
+        <div
+            className='slider-container'><ImageSlider slides={slides}></ImageSlider>
         </div>
-        </div>
+        </AnimatePresence>
 
     </>)
 }
