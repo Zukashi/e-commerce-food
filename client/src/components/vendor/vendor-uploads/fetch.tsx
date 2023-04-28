1bf605240a41cd4a33549649b9747713bcd512ea
Bartek <bartek.kaszowski@spoko.pl>
2023-04-28 13:17:05 +0200

    main img is requierd show toast when it's not selected

--- client/src/components/vendor/vendor-uploads/fetch.tsx
+++ client/src/components/vendor/vendor-uploads/fetch.tsx
@@ -1,16 +1,24 @@
 import {UploadProductFormValues} from "./UploadsForm";
 import axios, {AxiosInstance} from "axios";
 import {Images} from "../../../redux/vendor-uploads";
+import {toast} from "react-toastify";
 
 export const createProduct = async ({data, axiosPrivate, vendor}:{axiosPrivate:AxiosInstance,data:UploadProductFormValues, vendor:Images}) => {
     const formData = new FormData();
     /*
         * Add first image Main one
     */
-    const resMainImg = await axios.get(vendor.mainImage, {
-        responseType:"blob"
-    });
-    const imageFile = new File([resMainImg.data], 'image.jpg', { type: 'image/jpeg' });
+    let resMainImg
+    try{
+        resMainImg = await axios.get(vendor.mainImage, {
+            responseType:"blob"
+        });
+    }catch(e){
+        toast.error("Main image for product is required to proceed", {
+            theme:"dark"
+        })
+    }
+    const imageFile = new File([resMainImg?.data], 'image.jpg', { type: 'image/jpeg' });
     await formData.append("image", imageFile);
 
     // sub images
