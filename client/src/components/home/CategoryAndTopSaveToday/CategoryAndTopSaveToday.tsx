e09c808129a97a1ab5ea232d8d0e4518cdd8cbac
Bartek <bartek.kaszowski@spoko.pl>
2023-04-28 20:31:10 +0200

    CategoryAndTopSaveToday

---
+++ client/src/components/home/CategoryAndTopSaveToday/CategoryAndTopSaveToday.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Category } from './Category';
+import {TopProducts} from "./TopProducts";
+
+export const CategoryAndTopSaveToday = () => {
+    return (<main><Category/><TopProducts/></main>)
+}
