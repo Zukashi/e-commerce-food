6bc0c7881c361b9bb049910f5a5f0bf5588b53c9
Bartek <bartek.kaszowski@spoko.pl>
2023-04-29 14:30:46 +0200

    show product when image is loaded

--- client/src/components/home/CategoryAndTopSaveToday/OneProduct.tsx
+++ client/src/components/home/CategoryAndTopSaveToday/OneProduct.tsx
@@ -1,14 +1,19 @@
 import { motion } from 'framer-motion';
-import React from 'react';
+import React, {useState} from 'react';
 import {Product} from "../../../types/product";
 
 export const OneProduct = ({product, framerKey}:{product:Product, framerKey:URLSearchParams}) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    const handleImageLoad = () => {
+        setIsLoaded(true);
+    };
     return (<>
 
-          <motion.div key={framerKey.get('filter')} initial={{opacity:0, y:40}} animate={{opacity:1,y:0}}  className='product-container'>
+          <motion.div key={framerKey.get('filter')}  style={{opacity: isLoaded ? 1 :0  }} initial={{opacity:0, y:40 }} animate={{opacity:1, y:0}}   transition={{duration:0.4}} className='product-container'>
               <div className='product-content-container'>
                   <div className={'img-wrapper'}>
-                      <img src={product.productImages[0].imageUrl} alt={`Image of ${product.productName} product`}/>
+                      <img src={product.productImages[0].imageUrl} onLoad={handleImageLoad} alt={`Image of ${product.productName} product`}/>
                   </div>
                   <p>{product.category}</p>
                   <span>{product.productName}</span>
