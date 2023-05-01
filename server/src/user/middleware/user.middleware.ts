08a444793041a8a951fa1b8f8f9e066da2ae1f6a
Bartek <bartek.kaszowski@spoko.pl>
2023-05-01 16:47:27 +0200

    user middleware rebuild

--- server/src/user/middleware/user.middleware.ts
+++ server/src/user/middleware/user.middleware.ts
@@ -1,43 +1,45 @@
-// import {
-//   Injectable,
-//   NestMiddleware,
-//   NotFoundException,
-//   UnauthorizedException,
-// } from '@nestjs/common';
-// import { Request, Response, NextFunction } from 'express';
-// import { UserService } from '../user.service';
-// import { VendorService } from '../../vendor/vendor.service';
-//
-// @Injectable()
-// export class UserMiddleware implements NestMiddleware {
-//   constructor(
-//     private readonly userService: UserService,
-//     private readonly vendorService: VendorService,
-//   ) {}
-//
-//   async use(req: Request, res: Response, next: NextFunction) {
-//     const userRefreshCookie = req.cookies.refreshToken; // get user id from session
-//     console.log(userRefreshCookie, 'test');
-//     const user = await this.userService.findOne({
-//       value: userRefreshCookie,
-//       field: 'refresh_token',
-//     });
-//
-//     if (user?.refresh_token !== userRefreshCookie && user?.id) {
-//       console.log(22222);
-//       throw new NotFoundException();
-//     }
-//     if (user?.refresh_token !== userRefreshCookie) {
-//       const user = await this.vendorService.findOne({
-//         value: userRefreshCookie,
-//         field: 'refresh_token',
-//       });
-//       console.log(user);
-//       if (!user) throw new NotFoundException();
-//       req.user = user;
-//       next();
-//     } else {
-//       next();
-//     }
-//   }
-// }
+import { Injectable, NestMiddleware, NotFoundException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { Vendor } from '../../vendor/entities/vendor.entity';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+export interface ReqWithUser extends Request {
+  user: Vendor | User;
+}
+@Injectable()
+export class UserMiddleware implements NestMiddleware {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    @InjectRepository(Vendor)
+    private vendorRepository: Repository<Vendor>,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async use(req: ReqWithUser, res: Response, next: NextFunction) {
+    const userRefreshCookie = req.cookies.Refresh; // get user id from session
+    console.log(userRefreshCookie, 'test');
+    const decoded = this.jwtService.verify(userRefreshCookie, {
+      secret: this.configService.get('JWT_REFRESH_TOKEN_SECRET'),
+    });
+    const user = await this.userRepository.findOneBy({
+      id: decoded.userId,
+    });
+    if (user) {
+      req.user = user;
+      next();
+    }
+    const user2 = await this.vendorRepository.findOneBy({
+      id: decoded.userId,
+    });
+    if (user2?.refresh_token === userRefreshCookie && user2) {
+      req.user = user2;
+      next();
+    }
+    next();
+  }
+}
