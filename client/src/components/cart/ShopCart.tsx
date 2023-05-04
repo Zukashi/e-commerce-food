3ebfcdc0af5d26a678afe1e22d4ba1cfe95258aa
Bartek <bartek.kaszowski@spoko.pl>
2023-05-04 20:14:01 +0200

    ShopCart html structure

---
+++ client/src/components/cart/ShopCart.tsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import {useQuery} from "react-query";
+import {useAxiosPrivate} from "../../hooks/use-axios-private";
+import {AxiosInstance} from "axios";
+import {Product} from "../../types/product";
+import {Loader} from "../loader/Loader";
+import { OneProductInShopCart } from './OneProductInShopCart';
+import './cart-table.scss';
+
+const fetchCart = async (axios:AxiosInstance):Promise<Product[]> => {
+    const res = await axios.get('cart');
+    return res.data
+}
+export const ShopCart = () => {
+    const axiosPrivate = useAxiosPrivate();
+    const {data:products, isLoading} = useQuery('cart', () => fetchCart(axiosPrivate))
+    if(isLoading){
+        return <Loader/>
+    }
+    return(<><section style={{backgroundColor:'#fff'}}>
+        <div className='table-container'><div className='product-table'> <table> <tbody>{products?.map((product) => <tr><OneProductInShopCart product={product}key={product.id}/></tr>)}</tbody></table></div></div>
+
+    </section>
+    </>)
+}
