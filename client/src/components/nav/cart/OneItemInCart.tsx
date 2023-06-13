7215eb438df40c0fb33f6c90b1bea0eb5ebad4db
Bartek <bartek.kaszowski@spoko.pl>
2023-06-13 20:55:57 +0200

    delete product

--- client/src/components/nav/cart/OneItemInCart.tsx
+++ client/src/components/nav/cart/OneItemInCart.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import ClearIcon from "@mui/icons-material/Clear";
 import {Product} from "../../../types/product";
+import {useMutation, useQueryClient} from "react-query";
+import {useAxiosPrivate} from "../../../hooks/use-axios-private";
 
 export const  OneItemInCart = ({product}:{product:Product}) => {
+    const axiosPrivate = useAxiosPrivate();
+    const queryClient = useQueryClient();
+    const deleteProduct = useMutation((id:string) => {
+        return axiosPrivate.delete(`cart/product/${id}`);
+    },{
+        onSuccess:async () => {
+            await queryClient.invalidateQueries(['cart'])
+
+        }
+    });
     return (<> <div className='one-product-in-cart-container'>
         <div className='one-product-in-cart'>
             <img src={product.productImages[0].imageUrl} width={'60px'} height={'60px'} alt="test"/>
@@ -12,6 +24,6 @@ export const  OneItemInCart = ({product}:{product:Product}) => {
             </div>
 
         </div>
-        <ClearIcon fontSize={'small'}/>
+        <ClearIcon fontSize={'small'} onClick={() => deleteProduct.mutate(product.id)}/>
     </div></>)
 }
