a95c7e4faf6c54c1303b0c9b1572300b37af0a74
Bartek <bartek.kaszowski@spoko.pl>
2023-06-13 20:55:19 +0200

    cart should depend upon cookies not db data

--- server/src/cart/cart.service.ts
+++ server/src/cart/cart.service.ts
@@ -32,7 +32,7 @@ export class CartService {
   ) {}
   async getItems(req: ReqWithCustomer) {
     console.log(req.user);
-    if (req.user && req.cookies['cart']?.length < 1) {
+    if (req.user && req.cookies['cart']?.length < 0) {
       // If the user is logged in, retrieve the cart items from the database
       const cartItems = await this.cartRepository.find({
         where: { user: req.user },
@@ -83,130 +83,57 @@ export class CartService {
     if (addItemDto.quantity > product?.quantity) {
       throw new ConflictException('Too high quantity');
     }
-    console.log(req.cookies);
-    if (req.user && req.cookies['cart']?.length < 1) {
-      // If the user is logged in, retrieve the cart items from the database
-      let cart = await this.cartRepository
-        .createQueryBuilder('cart')
-        .leftJoinAndSelect('cart.user', 'user')
-        .leftJoinAndSelect('cart.cartItems', 'cartItems')
-        .leftJoinAndSelect('cartItems.product', 'product')
-        .where('user.id =:userId', { userId: req.user.id })
-        .getOne();
-      if (!cart) {
-        // If cart does not exist, create a new one
-        cart = await this.cartRepository.create({
-          user: req.user,
-          cartItems: [],
-        });
-        await this.cartRepository.save(cart);
-        const cartItem = await this.cartItemRepository.create({
-          cart,
-          product,
-          quantity: addItemDto.quantity,
-        });
-        console.log(cartItem);
-        await this.cartItemRepository.save(cartItem);
-        cart.cartItems.push(cartItem);
-      } else {
-        // If cart exists, add the new product to the existing products
-        const cartItem = await this.cartItemRepository
-          .createQueryBuilder('cartItem')
-          .leftJoinAndSelect('cartItem.product', 'product')
-          .where('product.id =:id', {
-            id: productId,
-          })
-          .getOne();
-        console.log(cartItem, 999999);
-        if (!cartItem) {
-          const cartItem = await this.cartItemRepository.create({
-            cart,
-            product,
-            quantity: addItemDto.quantity,
-          });
-          cart.cartItems.push(cartItem);
-          await this.cartItemRepository.save(cartItem);
-        } else {
-          const cartItemNew = await this.cartItemRepository
-            .createQueryBuilder('cartItem')
-            .leftJoinAndSelect('cartItem.product', 'product')
-            .leftJoinAndSelect('cartItem.cart', 'cart')
-            .update(CartItem)
-            .set({
-              quantity: addItemDto.quantity,
-            })
-            .where('product.id = :productId', { productId })
-            .andWhere('cart.id = :cartId', { cartId: cart.id })
-            .execute();
-          if (!cartItemNew) throw new NotFoundException();
-        }
-      }
-
-      const cartUpdated = await this.cartRepository.findOne({
-        where: {
-          id: cart.id,
-        },
-        relations: {
-          cartItems: {
-            product: true,
-          },
-          user: true,
-        },
+    // If the user is not logged in, retrieve the cart items from the cookie
+    let cartItemsAsProductIds: { productId: string; quantity: number }[] =
+      req.cookies['cart'] || [];
+    console.log(cartItemsAsProductIds.length);
+    if (!Boolean(cartItemsAsProductIds.length)) {
+      console.log(9999);
+      cartItemsAsProductIds.push({
+        productId,
+        quantity: addItemDto.quantity,
       });
-      return cartUpdated;
-    } else {
-      // If the user is not logged in, retrieve the cart items from the cookie
-      let cartItemsAsProductIds: { productId: string; quantity: number }[] =
-        req.cookies['cart'] || [];
-      console.log(cartItemsAsProductIds.length);
-      if (!Boolean(cartItemsAsProductIds.length)) {
-        console.log(9999);
-        cartItemsAsProductIds.push({
-          productId,
-          quantity: addItemDto.quantity,
-        });
-        res.cookie(
-          'cart',
-          [
-            {
-              productId,
-              quantity: addItemDto.quantity,
-            },
-          ],
+      res.cookie(
+        'cart',
+        [
           {
-            expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7),
-            secure: true,
-            httpOnly: true,
+            productId,
+            quantity: addItemDto.quantity,
           },
-        );
-      } else {
-        if (
-          cartItemsAsProductIds.some((cartItem) => {
-            return cartItem.productId === productId;
-          })
-        ) {
-          console.log(cartItemsAsProductIds);
-          /// if productId already exists in cartItems then delete and replace
-          cartItemsAsProductIds = cartItemsAsProductIds.filter((cartItem) => {
-            return cartItem.productId !== productId;
-          });
-          console.log(cartItemsAsProductIds);
-          cartItemsAsProductIds.push({
-            quantity: addItemDto.quantity,
-            productId,
-          });
-        } else {
-          cartItemsAsProductIds.push({
-            quantity: addItemDto.quantity,
-            productId,
-          });
-        }
-        res.cookie('cart', [...cartItemsAsProductIds], {
+        ],
+        {
           expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7),
           secure: true,
           httpOnly: true,
+        },
+      );
+    } else {
+      if (
+        cartItemsAsProductIds.some((cartItem) => {
+          return cartItem.productId === productId;
+        })
+      ) {
+        console.log(cartItemsAsProductIds);
+        /// if productId already exists in cartItems then delete and replace
+        cartItemsAsProductIds = cartItemsAsProductIds.filter((cartItem) => {
+          return cartItem.productId !== productId;
+        });
+        console.log(cartItemsAsProductIds);
+        cartItemsAsProductIds.push({
+          quantity: addItemDto.quantity,
+          productId,
+        });
+      } else {
+        cartItemsAsProductIds.push({
+          quantity: addItemDto.quantity,
+          productId,
         });
       }
+      res.cookie('cart', [...cartItemsAsProductIds], {
+        expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7),
+        secure: true,
+        httpOnly: true,
+      });
 
       const cartItems = await this.productRepository.find({
         where: cartItemsAsProductIds.map((cartItem) => ({
@@ -234,36 +161,17 @@ export class CartService {
     const product = await this.productRepository.findOneBy({
       id: productId,
     });
-    if (req.user) {
-      // If the user is logged in, retrieve the cart items from the database
-      const cart = await this.cartRepository
-        .createQueryBuilder('cart')
-        .leftJoinAndSelect('cart.user', 'user')
-        .leftJoinAndSelect('cart.cartItems', 'cartItems')
-        .leftJoinAndSelect('cartItems.product', 'product')
-        .where('user.id =:userId', { userId: req.user.id })
-        .getOne();
 
-      if (!cart) throw new NotFoundException();
-      await this.cartItemRepository
-        .createQueryBuilder('cartItem')
-        .leftJoinAndSelect('cartItem.product', 'product')
-        .delete()
-        .from(CartItem)
-        .where('product.id =:productId', { productId })
-        .execute();
-    } else {
-      const cartItemsAsProductIds: { productId: string; quantity: number }[] =
-        req.cookies['cart'] || [];
-      const filteredProducts = cartItemsAsProductIds.filter((cartItem) => {
-        return cartItem.productId !== productId;
-      });
-      response.cookie('cart', filteredProducts, {
-        expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7),
-        secure: true,
-        httpOnly: true,
-      });
-    }
+    const cartItemsAsProductIds: { productId: string; quantity: number }[] =
+      req.cookies['cart'] || [];
+    const filteredProducts = cartItemsAsProductIds.filter((cartItem) => {
+      return cartItem.productId !== productId;
+    });
+    response.cookie('cart', filteredProducts, {
+      expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7),
+      secure: true,
+      httpOnly: true,
+    });
   }
 
   async removeElementFromCart(
