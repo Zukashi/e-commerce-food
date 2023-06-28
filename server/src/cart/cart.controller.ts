8c8fee24376ebbdcacbc050f8424ab74e1d26acd
Bartek <bartek.kaszowski@spoko.pl>
2023-06-28 14:05:46 +0200

    patch quantity of cart item

--- server/src/cart/cart.controller.ts
+++ server/src/cart/cart.controller.ts
@@ -13,6 +13,7 @@ import { ReqWithCustomer } from '../auth/types/Req/User';
 import { CartService } from './cart.service';
 import { AddItemDto } from './dto/AddItem.dto';
 import { Response } from 'express';
+import { ChangeItemDto } from './dto/ChangeItem.dto';
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
@@ -21,7 +22,20 @@ export class CartController {
   async getCartItems(@Req() req: ReqWithCustomer) {
     return this.cartService.getItems(req);
   }
-
+  @Patch('/product/:productId/quantity')
+  async changeQuantityOfProductInCart(
+    @Req() req: ReqWithCustomer,
+    @Body() changeItemDto: ChangeItemDto,
+    @Param('productId') productId: string,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return this.cartService.changeItemQuantityInCart(
+      req,
+      changeItemDto,
+      productId,
+      res,
+    );
+  }
   @Post('/product/:productId')
   async addItemToCart(
     @Req() req: ReqWithCustomer,
