6b2f8f5d5c5e64d71811191060c6dff50e899d83
Bartek <bartek.kaszowski@spoko.pl>
2023-07-01 14:08:53 +0200

    get and map best sellers products

--- client/src/components/home/DailyBestSellers/DailyBestSellers.tsx
+++ client/src/components/home/DailyBestSellers/DailyBestSellers.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import './scss/DailyBestSellers.scss'
+import {Rating} from "@mui/material";
+import {motion} from "framer-motion";
+import {useQuery} from "react-query";
+import {useAxiosPrivate} from "../../../hooks/use-axios-private";
+import {AxiosInstance} from "axios";
+import {Product} from "../../../types/product";
+import {Loader} from "../../loader/Loader";
+import {OneBestSeller} from "./OneBestSeller";
+const fetchBestSellers = async (axios:AxiosInstance):Promise<Product[]> => {
+    const res = await axios.get('product/best');
+    return res.data
+}
 export const DailyBestSellers  = () => {
+    const axiosPrivate = useAxiosPrivate()
+    const {data, refetch, isLoading, isRefetching,isFetching } = useQuery('products/best', () => fetchBestSellers(axiosPrivate));
+    if(!data){
+        return <Loader/>
+    }
     return <>
         <h2 className='best-sellers-header'>Daily Best Sellers</h2>
-
+        {data.map((product) => <OneBestSeller key={product.id} product={product}/>)}
     </>
 }
---
+++ client/src/components/home/DailyBestSellers/OneBestSeller.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Rating} from "@mui/material";
+import {motion} from "framer-motion";
+import {Product} from "../../../types/product";
+
+export const OneBestSeller = ({product}:{product:Product}) => {
+    console.log(product)
+    return ( <>
+        <div className='best-product-container'>
+            <div className='best-product-content-container'>
+                <div className='best-image-wrapper'>
+                    <img
+                        src={product.productImages[0].imageUrl}
+
+                        alt={`Image of ${product.productName} product`}
+                        style={{    height:'255px', width:'240px' }}
+                    />
+                </div>
+                <p className='best-product-category'>{product.category}</p>
+                <p className='best-product-name'>{product.productName}</p>
+                <div ><Rating size={"small"}   name="read-only" value={4} readOnly /></div>
+                <p className='best-product-vendor'>By {product.vendor.username}</p>
+                <div className='best-product-price-add-container'>
+                    <p>{product.price}</p>
+                    <div>
+
+                        <motion.button  className='best-product-button' >
+                            <i className="fa-solid fa-cart-shopping"></i> Add</motion.button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>)
+}
