49d791a5452ca4308a0ba29d367646c03047828e
Bartek <bartek.kaszowski@spoko.pl>
2023-08-11 22:26:22 +0200

    CheckoutForm.tsx

---
+++ client/src/components/cart/CheckoutForm.tsx
@@ -0,0 +1,91 @@
+import {
+    PaymentElement,
+    LinkAuthenticationElement,
+    AddressElement,
+    useStripe,
+    useElements
+} from "@stripe/react-stripe-js";
+const fetchCart = async (axios:AxiosInstance):Promise<Product[]> => {
+    const res = await axios.get('cart');
+    return res.data
+}
+import {useEffect, useState} from "react";
+import {useAxiosPrivate} from "../../hooks/use-axios-private";
+import {useQuery} from "react-query";
+import {AxiosInstance} from "axios";
+import {Product} from "../../types/product";
+export const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const axiosPrivate = useAxiosPrivate();
+    const {data:products} = useQuery('cart', () => fetchCart(axiosPrivate))
+    const [message, setMessage] = useState<null | string>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const arrSum = products?.reduce((acc, currentValue) => acc + currentValue.price * currentValue.quantity, 0)
+    useEffect(() => {
+        if (!stripe) {
+            return;
+        }
+
+        const clientSecret = new URLSearchParams(window.location.search).get(
+            "payment_intent_client_secret"
+        );
+        if (!clientSecret) {
+            return;
+        }
+
+        stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+            if(!paymentIntent){
+                return null
+            }
+            console.log(paymentIntent.amount)
+            switch (paymentIntent.status) {
+                case "succeeded":
+                    setMessage("Payment succeeded!");
+                    break;
+                case "processing":
+                    setMessage("Your payment is processing.");
+                    break;
+                case "requires_payment_method":
+                    setMessage("Your payment was not successful, please try again.");
+                    break;
+                default:
+                    setMessage("Something went wrong.");
+                    break;
+            }
+        });
+    }, [stripe]);
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            // Stripe.js has not yet loaded.
+            // Make sure to disable form submission until Stripe.js has loaded.
+            return;
+        }
+
+        setIsLoading(true);
+
+        // const response = await stripe.confirmPayment({
+        //     elements,
+        //     confirmParams: {
+        //         return_url: "http://localhost:5173",
+        //     },
+        // });
+        setIsLoading(false);
+    };
+
+
+    return (
+        <form id="payment-form" onSubmit={handleSubmit}>
+
+            <PaymentElement id="payment-element"  />
+            <button disabled={isLoading || !stripe || !elements} id="submit">
+        <span id="button-text">
+          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+        </span>
+            </button>
+            {message && <div id="payment-message">{message}</div>}
+        </form>
+    );
+}
