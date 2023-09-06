b830efb1151275fa063f79ed4ff71835829acd35
Bartek <bartek.kaszowski@spoko.pl>
2023-09-06 19:38:11 +0200

    order entity

--- server/src/order/entities/order.entity.ts
+++ server/src/order/entities/order.entity.ts
@@ -1,17 +1,35 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 import { User } from '../../user/entities/user.entity';
+import {
+  Product,
+  ProductCategoryEnum,
+} from '../../product/entities/product.entity';
+import { Vendor } from '../../vendor/entities/vendor.entity';
 
 @Entity()
 export class Order {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({
-    unique: true,
-  })
-  productName: string;
+  @Column()
+  orderId: number;
 
-  @ManyToOne(() => User, (user) => user.orders)
+  @ManyToOne(() => User, (user) => user.orders, { nullable: true })
   user: User;
+
+  @ManyToOne(() => Vendor, (vendor) => vendor.orders, { nullable: true })
+  vendor: Vendor;
+
+  @ManyToMany(() => Product)
+  @JoinTable()
+  products: Product[];
 }
