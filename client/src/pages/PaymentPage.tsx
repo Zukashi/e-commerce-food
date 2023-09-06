37c016d665135b6126cd6344f4ff57cdb5b11ff5
Bartek <bartek.kaszowski@spoko.pl>
2023-09-06 19:38:41 +0200

    try catch call payment, create ordre

--- client/src/pages/PaymentPage.tsx
+++ client/src/pages/PaymentPage.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
+
 const stripePromise = loadStripe('pk_test_51NJOH7F6ApijUKU4FS1r5tlrVciPQKKGoKSWdjjshBrzY5RZEFGWmM4fmXGOVlFHeOQCdzbvmbuABZ3wEa02h9wZ00DVvuvc9V');
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
+import {loadStripe} from "@stripe/stripe-js";
+import {Elements} from "@stripe/react-stripe-js";
 import {CheckoutForm} from "../components/cart/CheckoutForm";
 import {useAxiosPrivate} from "../hooks/use-axios-private";
 import {Loader} from "../components/loader/Loader";
@@ -10,7 +11,7 @@ import {AxiosInstance} from "axios/index";
 import {Product} from "../types/product";
 import {useQuery} from "react-query";
 
-const fetchCart = async (axios:AxiosInstance):Promise<Product[]> => {
+const fetchCart = async (axios: AxiosInstance): Promise<Product[]> => {
     const res = await axios.get('cart');
     return res.data
 }
@@ -20,28 +21,39 @@ export const PaymentPage = () => {
     let [searchParams, setSearchParams] = useSearchParams();
     const axiosPrivate = useAxiosPrivate();
     const location = useLocation();
-    const {data:products, isLoading, isFetching} = useQuery('cart', () => fetchCart(axiosPrivate))
+    const {data: products, isLoading, isFetching} = useQuery('cart', () => fetchCart(axiosPrivate))
     const createOrder = async () => {
-        const res = await axiosPrivate.post("order", products)
-    }
-    const callPayment = async() => {
-        const res = await axiosPrivate.post("stripe/create-payment-intent", {arrSum:location.state})
-        setClientSecret(res.data)
-        setSearchParams({payment_intent_client_secret:res.data})
-    }
+        try {
+            console.log(products);
+            const res = await axiosPrivate.post("order", {products});
+            console.log("Order created", res.data);
+        } catch (error) {
+            console.error("Error creating order:", error);
+        }
+    };
+    const callPayment = async () => {
+        try {
+            const res = await axiosPrivate.post("stripe/create-payment-intent", {arrSum: location.state});
+            await createOrder();
+            setClientSecret(res.data);
+            setSearchParams({payment_intent_client_secret: res.data});
+        } catch (error) {
+            console.error("Error in payment intent:", error);
+        }
+    };
     useEffect(() => {
         !clientSecret && callPayment()
     }, [])
     const options = {
         clientSecret,
     };
-    if(!clientSecret){
+    if (!clientSecret || !products) {
         return <Loader/>
     }
     return (<>
         {clientSecret && (
             <Elements options={options} stripe={stripePromise}>
-                <CheckoutForm />
+                <CheckoutForm/>
             </Elements>
         )}
     </>)
