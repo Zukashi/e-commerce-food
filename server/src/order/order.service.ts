0bfe27f0a194fc72082fd6e0b8088adc85ace35f
Bartek <bartek.kaszowski@spoko.pl>
2023-09-06 19:38:26 +0200

    add order, generate order id

--- server/src/order/order.service.ts
+++ server/src/order/order.service.ts
@@ -3,6 +3,8 @@ import { ReqWithVendor } from '../auth/types/Req/Vendor';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Repository } from 'typeorm';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { ReqWithUser } from '../user/middleware/user.middleware';
 
 @Injectable()
 export class OrderService {
@@ -11,7 +13,7 @@ export class OrderService {
     private orderRepository: Repository<Order>,
   ) {}
 
-  async getOrdersOfVendor(req: ReqWithVendor) {
+  async getOrdersOfVendor(req: ReqWithVendor): Promise<Order[]> {
     return this.orderRepository.find({
       relations: {
         user: true,
@@ -20,5 +22,18 @@ export class OrderService {
         user: req.user,
       },
     });
+  }
+  generateOrderId(): number {
+    return Math.floor(100000 + Math.random() * 900000);
+  }
+  async addOrder(req: ReqWithUser, createOrderDto: CreateOrderDto) {
+    console.log('User:', req.user);
+    console.log('Products:', createOrderDto.products);
+    const order = this.orderRepository.create({
+      vendor: req.user,
+      orderId: this.generateOrderId(),
+      products: createOrderDto.products,
+    });
+    return this.orderRepository.save(order);
   }
 }
