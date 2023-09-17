5ae46a60461ef1d419caf8b08a4f1251ea520f0a
Zukashi <bartek.kaszowski@spoko.pl>
2023-09-17 18:22:41 +0200

    remove unused constants

--- server/src/auth/constants.ts
+++
@@ -1,6 +0,0 @@
-export const jwtConstants = {
-  access_token_secret:
-    'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
-  refresh_token_secret:
-    'sdkfjhusidoap809fu9832574fuj45089fy4h359fhj345f93457yg453h0f94hu5f54633',
-};
